5886d0006eee8d737db997d3d326ece38de1f6c9
FLYEMOJ1 <admin@flyemoji.xyz>
2022-05-29 21:15:53 +0900

    Update favicon & icon & site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,14 +3,14 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: 'https://',
   domain: (import.meta.env.URARA_SITE_DOMAIN as string) ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'FlyEmoji Blog',
+  subtitle: '是的没错，老子又回来了',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: 'Powered by SvelteKit/Urara & Vercel & Cloudflare',
   author: {
-    name: 'Minseong Choi',
-    photo: '/assets/maskable@192.png',
-    status: '🌸',
+    name: 'FLYEMOJI',
+    photo: '/assets/me@192.png',
+    status: '🖤',
     bio: '当世界舍弃了你，不还是有我么。'
   },
   themeColor: '#3D4451'
